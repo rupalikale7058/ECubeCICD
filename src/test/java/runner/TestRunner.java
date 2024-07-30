bada5be99fc9d511e5bac2142dc8a6b2da3497c9
rupalikale7058 <rupali.kale@rysun.com>
2024-07-30 13:14:40 +0530

    Updated script for Member Module related test cases new

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import util.ReportLogger;
 
 @CucumberOptions(features = "src/test/resources/features", glue = {"stepdefinations",
         "hooks"}, publish = true, plugin = {"progress", "html:target/cucumber-reports/cucumber-report.html",
-        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = "@TestCase-ROO009")
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, tags = "@REG")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
     ReportLogger reportLogger = ReportLogger.getInstance();
